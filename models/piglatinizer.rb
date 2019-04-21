f0e30250cdcdea3fc95a02f6ae1e30733a1ab64f
Heather Kirk <hskirk@gmail.com>
2019-04-21 23:39:32 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,22 +6,23 @@ class PigLatinizer
   b.join(" ")
   end 
   
-  # def piglatinize_word(word)
-  #   first_letter = word[0].downcase
-  #   if ["a","e","i","o","u"].include?(first_letter)
-  #     "#{word}way"
-  #   else 
-  #     consonants = []
-  #     consonants << word[0]
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
+    if ["a","e","i","o","u"].include?(first_letter)
+      "#{word}way"
+    else 
+      consonants = []
+      consonants << word[0]
       
-  #     if ["a","e","i","o","u"].include? == false
-  #       consonants << word[1]
+      if ["a","e","i","o","u"].include? == false
+        consonants << word[1]
         
-  #     if ["a","e","i","o","u"].include? == false
-  #       consonants << word[2]
+      if ["a","e","i","o","u"].include? == false
+        consonants << word[2]
         
-  #     end 
-  #   end 
-  #       "#{word[consonants.length..-1] + consonants.join + "ay"}"
-  #   end 
+      end 
+    end 
+        "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end 
+  end 
 end 
